b556aab5d9f4e5117bc9cb918a9dc252220921d7
Bijoya Brakenhoff <bbrakenhoff@gmail.com>
2024-11-10 18:43:00 +0100

    Get all retrospective elements I can use in a retrospective.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ElementsOverviewComponent} from './elements-overview/elements-overview.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ElementsOverviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
